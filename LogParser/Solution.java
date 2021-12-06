acb3c069b9aa46f8d54d007a175b93f4d89a1379
AAZaykov <vya8585@mail.ru>
2021-12-06 16:14:28 +0500

    ! line added to Solution

--- LogParser/Solution.java
+++ LogParser/Solution.java
@@ -8,5 +8,7 @@ public class Solution {
     public static void main(String[] args) throws IOException, ParseException {
         LogParser logParser = new LogParser(Paths.get("d:/JAVA/logs/"));
         System.out.println(logParser.execute("get ip for user = \"Eduard Petrovich Morozko\""));
+        System.out.println(logParser.execute("get ip"));
+        System.out.println(logParser.execute("get user"));
     }
 }
